670a45df284a978f7f629fa7766d2cadc0eb2213
pchatard <chatard.pierre@gmail.com>
2021-01-24 16:01:01 +0100

    Test

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
 
     // Axios module configuration (https://go.nuxtjs.dev/config-axios)
     axios: {
-        baseURL: 'http://localhost:3000',
+        // baseURL: 'http://localhost:3000',
     },
 
     auth: {
