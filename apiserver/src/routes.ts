c10a1d4207971b225f4994b0a78a7d0b651cc13a
ndureiss <nicolas.dureeisseix@hotmail.fr>
2022-01-16 00:55:20 +0100

    change method for invite endpoint

--- apiserver/src/routes.ts
+++ apiserver/src/routes.ts
@@ -338,7 +338,7 @@ export function RegisterRoutes(app: express.Router) {
             }
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.get('/lists/invite/:sharingCode',
+        app.put('/lists/invite/:sharingCode',
             authenticateMiddleware([{"auth0":[]}]),
 
             function ListController_accessFromSharingCode(request: any, response: any, next: any) {
