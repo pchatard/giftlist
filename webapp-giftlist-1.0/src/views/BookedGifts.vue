3c41cebffad5645b9ffa73fe0f2364f04c802b5c
Pierre Chatard <chatard.pierre@gmail.com>
2022-01-16 21:35:40 +0100

    BookedGifts : Create page and link router

---
+++ webapp-giftlist-1.0/src/views/BookedGifts.vue
@@ -0,0 +1,15 @@
+<template>
+	<DefaultLayout title="Mes cadeaux réservés"></DefaultLayout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import DefaultLayout from "@/components/Styled/DefaultLayout.vue";
+
+export default defineComponent({
+	name: "BookedGifts",
+	components: {
+		DefaultLayout,
+	},
+});
+</script>
