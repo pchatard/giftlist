20fff36897397c41ccd6d8654b81b0e4ad672823
Pierre Chatard <chatard.pierre@gmail.com>
2022-01-16 21:11:03 +0100

    Prettier

--- webapp-giftlist-1.0/src/types/api/Gift.ts
+++ webapp-giftlist-1.0/src/types/api/Gift.ts
@@ -4,19 +4,19 @@ import { List } from "./List";
 import { User } from "./User";
 
 export interface Gift {
-    readonly id?: string;
-    title: string;
-    isFavorite: boolean;
-    category: GiftCategory;
-    lists?: List[];
-    price?: number;
-    link?: string;
-    brand?: string;
-    size?: string;
-    color?: string;
-    comments?: string;
-    isHidden?: boolean;
-    isBooked: boolean;
-    bookedBy?: User[];
-    bookings?: GiftBooking[];
+	readonly id?: string;
+	title: string;
+	isFavorite: boolean;
+	category: GiftCategory;
+	lists?: List[];
+	price?: number;
+	link?: string;
+	brand?: string;
+	size?: string;
+	color?: string;
+	comments?: string;
+	isHidden?: boolean;
+	isBooked: boolean;
+	bookedBy?: User[];
+	bookings?: GiftBooking[];
 }
--- webapp-giftlist-1.0/src/types/api/GiftCategory.ts
+++ webapp-giftlist-1.0/src/types/api/GiftCategory.ts
@@ -1,4 +1,4 @@
 export interface GiftCategory {
-    id: string;
-    name: string;
+	id: string;
+	name: string;
 }
