5a7227b6c61b954d8c1d0b7de697e12b396eac52
Pierre Chatard <chatard.pierre@gmail.com>
2022-03-10 16:08:19 +0100

    Update dependencies

--- webapp-giftlist-1.0/.eslintrc.js
+++ webapp-giftlist-1.0/.eslintrc.js
@@ -6,9 +6,8 @@ module.exports = {
 	extends: [
 		"plugin:vue/vue3-essential",
 		"eslint:recommended",
-		"@vue/typescript/recommended",
-		"@vue/prettier",
-		"@vue/prettier/@typescript-eslint",
+		'prettier',
+		"@vue/typescript/recommended"
 	],
 	parserOptions: {
 		ecmaVersion: 2020,
@@ -16,6 +15,8 @@ module.exports = {
 	rules: {
 		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
 		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-unsafe-optional-chaining": "warn",
+		"vue/multi-word-component-names": "off"
 	},
 	overrides: [
 		{
