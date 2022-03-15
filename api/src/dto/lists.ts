633a2a0a7a6c5aac89da3cf26a2158982844303b
ndureiss <nicolas.dureisseix@hotmail.fr>
2022-03-15 23:14:36 +0100

    add description and auth0Id is no more user id

--- api/src/dto/lists.ts
+++ api/src/dto/lists.ts
@@ -9,9 +9,11 @@ export interface CreateListDTO
 		>
 	> {}
 export interface ListIdDTO extends Expand<Pick<List, "id">> {}
-export interface ListDTO
-	extends Expand<
-		Pick<List, "id" | "title" | "isShared" | "sharingCode" | "closureDate" | "ownersIds">
-	> {}
+
+// TODO: Understand why Expand make tsoa going to timeout
+export type ListDTO = Pick<
+	List,
+	"id" | "title" | "isShared" | "sharingCode" | "closureDate" | "ownersIds" | "grantedUsersIds"
+>;
 
 export interface EditListDTO extends Expand<Omit<ListDTO, "id">> {}
