28cc334dcf4d4b2b5f0d17fb0efab04177ceaf3a
Pierre <39740196+pchatard@users.noreply.github.com>
2022-03-20 17:20:31 +0100

    List sharing modal (#91)
    * Create basic modal with contents
    
    * Style modal + small fixes

--- webapp-giftlist-1.0/src/types/ButtonStyleEnum.ts
+++ webapp-giftlist-1.0/src/types/ButtonStyleEnum.ts
@@ -1,5 +1,8 @@
 export enum ButtonStyleEnum {
 	primary = "primary",
+	primarySoft = "primary-soft",
 	secondary = "secondary",
 	danger = "danger",
+	dangerSoft = "danger-soft",
+	greenSoft = "green-soft"
 }
