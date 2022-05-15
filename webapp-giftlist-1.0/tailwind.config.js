fdaa252263df98aafb08f39ae1bb4ced37e11722
Pierre Chatard <chatard.pierre@gmail.com>
2022-05-15 17:12:23 +0200

    Update tailwindcss to last version

--- webapp-giftlist-1.0/tailwind.config.js
+++ webapp-giftlist-1.0/tailwind.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-	purge: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			colors: {},
-			minHeight: {
-				layout: "calc(100vh - 15rem)",
-			},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
-};
+  content: [
+    "./public/index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      minHeight: {
+        layout: "calc(100vh - 15rem)",
+      },
+    },
+  },
+  plugins: [],
+}
