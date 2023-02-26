11845303ea4ee3f3d0008a029af6496e2964a44d
Pierre Chatard <chatard.pierre@gmail.com>
2023-02-26 16:50:38 +0100

    Fix build

--- client/src/injectionSymbols.ts
+++ client/src/injectionSymbols.ts
@@ -1,8 +1,6 @@
 import type { InjectionKey } from "vue";
 import type { BreadcrumbContentData, DarkModeData } from "./types";
-import type { UserInjectionData } from "./types/users";
 
-export const userInjectionKey = Symbol() as InjectionKey<UserInjectionData>;
 export const darkModeInjectionKey = Symbol() as InjectionKey<DarkModeData>;
 export const breadcrumbContentInjectionKey =
   Symbol() as InjectionKey<BreadcrumbContentData>;
